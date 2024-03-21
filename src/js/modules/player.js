7714d67454b556cb59024cfebc37e73efd00fc53
DmitryKonovets0 <dmitrykonovets@gmail.com>
2024-03-21 20:14:26 +0200

    Add functionality for player + fix bug

--- src/js/modules/player.js
+++ src/js/modules/player.js
@@ -3,15 +3,41 @@ export default class Player {
     this.btns = document.querySelectorAll(btn)
     this.overlay = document.querySelector(overlay)
     this.close = this.overlay.querySelector('.close')
+    this.t = 0
     }
     createPlayer(url){
         this.player =  new YT.Player('frame',  {
             height: '100%',
             width: '100%',
-            videoId: `${url}`
+            videoId: `${url}`,
         });
-
         this.overlay.style.display = 'flex'
+        this.t = 1
+
+    }
+    bindTriggers() {
+        this.btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                const path = btn.getAttribute('data-url');
+                if(this.t === 0) {
+                    this.createPlayer(path)
+                }else {
+                    this.overlay.style.display = 'flex'
+                }
+            })
+        })
+    }
+    bindCloseBtn() {
+        this.close.addEventListener('click', () => {
+            this.overlay.style.display = 'none'
+            this.player.stopVideo()
+        })
+        this.overlay.addEventListener('click', () => {
+            this.overlay.style.display = 'none'
+            this.player.stopVideo()
+
+
+        })
     }
     render() {
         const tag = document.createElement('script');
@@ -19,12 +45,7 @@ export default class Player {
         tag.src = "https://www.youtube.com/iframe_api";
         const firstScriptTag = document.getElementsByTagName('script')[0];
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        this.btns.forEach(btn => {
-            btn.addEventListener('click', () => {
-                const path = btn.getAttribute('data-url');
-
-                this.createPlayer(path)
-            })
-        })
+        this.bindTriggers()
+        this.bindCloseBtn()
     }
 }
